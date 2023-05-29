eed2dcb640f58850290a49e3979994b994ad0191
Misha Bilash <novasupport@yandex.ru>
2023-05-29 16:54:07 +0300

    logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,31 @@
 const openPopupButtonEl = document.querySelector('#open-popup-button');
 const closePopupButtonEl = document.querySelector('#close-popup-button');
+
+
+const openPopupAddButtonEl = document.querySelector('#open-add-popup-button');
+const closePopupAddButtonEl = document.querySelector('#close-add-popup-button');// две строки ,добавил новые переменные. селектор ищет по моему id в html
+
+
 const editPopupEl = document.querySelector('#edit-popup');
+const addPopupEl = document.querySelector('#add-popup');
+
 const pageTitleEl = document.querySelector('.profile__name');
 const pageSubtitleEl = document.querySelector('.profile__description');
 const nameInputEl = document.querySelector('#popup__name-input');
 const subnameInputEl = document.querySelector('#popup__subname-input');
+
+const pageTextEl = document.querySelector('.element__name');
+const pageImageEl = document.querySelector('.element__photo');
+const textInputEl = document.querySelector('#popup__text-input');//повторил константы, только надо их править, но смысл поймешь, выше это мы делали для профиля
+const imageInputEl = document.querySelector('#popup__image-input');
+
 const editFormEl = document.querySelector('#edit-form');
 
 openPopupButtonEl.addEventListener('click', function () {
   openPopup(editPopupEl);
   nameInputEl.value = pageTitleEl.textContent;
 subnameInputEl.value = pageSubtitleEl.textContent;
+
 });
 
 closePopupButtonEl.addEventListener('click', function () {
@@ -35,3 +50,40 @@ function openPopup(popupEl) {
 function closePopup(popupEl) {
   popupEl.classList.remove('popup_opened');
 }
+
+
+
+//снизу повторяю функцию как изменение имени и описания, но подставляю свои кнопки,переменные
+
+
+
+openPopupAddButtonEl.addEventListener('click', function () {
+  openPopup(addPopupEl);
+  textInputEl.value = pageTextEl.textContent;
+imageInputEl.value = pageImageEl.textContent;
+
+});
+
+closePopupAddButtonEl.addEventListener('click', function () {
+  closePopup(addPopupEl);
+});
+
+
+
+editFormEl.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  pageTextEl.textContent = textInputEl.value;
+  pageImageEl.textContent = imageInputEl.value;
+
+  closePopup(addPopupEl);
+});
+
+function openPopup(popupEl) {
+  popupEl.classList.add('popup_opened');
+}
+
+function closePopup(popupEl) {
+  popupEl.classList.remove('popup_opened');
+}
+
